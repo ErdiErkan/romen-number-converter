4f7dfd46587739c47bc9e38c58683e8e11f8579c
Erdi ERKAN <erdi.erkan@akdemir.corp>
2023-09-20 15:45:47 +0300

    cli

---
+++ userdata.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+yum update -y
+yum install python3 -y
+yum install python3-pip -y
+pip3 install flask
+yum install git -y
+cd /home/ec2-user
+wget -P templates https://raw.githubusercontent.com/ErdiErkan/romen-number-converter/main/templates/index.html
+wget -P templates https://raw.githubusercontent.com/ErdiErkan/romen-number-converter/main/templates/result.html
+wget https://raw.githubusercontent.com/ErdiErkan/romen-number-converter/main/app.py
+python3 app.py
